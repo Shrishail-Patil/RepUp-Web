312bc28f47dbfc56c768c014b201e499ca42a41d
ShrishailPatil <apple@Shrishails-MacBook-Air.local>
2025-01-02 20:22:04 +0530

    login redirect changed

--- app/pre-auth/Login/page.tsx
+++ app/pre-auth/Login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
             access_type: 'offline',
             prompt: 'consent',
           },      
-          redirectTo: `${window.location.origin}/auth/Profile`, // Redirect after successful login
+          redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/Profile`, // Redirect after successful login
         },
       });
 
